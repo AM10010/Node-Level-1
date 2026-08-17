ab30ffea9872f944fda5a4a13c00273c31a26373
AM10010 <mabanoub826@gmail.com>
2026-08-17 14:42:18 +0300

    Lesson 2

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose')
+
+
 
 app.get('/', (req, res) => {
   res.sendFile("./views/home.html", { root: __dirname });
 });
 
-app.listen(port, () => {
+
+
+mongoose.connect('mongodb+srv://mabanoub826_db_user:X1DLOdnTIxCYF373@cluster0.4vbiftl.mongodb.net/all-data?appName=Cluster0').then(() => {
+ app.listen(port, () => {
   console.log(`http://localhost:${port}/`);
+});
+}).catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
 });
